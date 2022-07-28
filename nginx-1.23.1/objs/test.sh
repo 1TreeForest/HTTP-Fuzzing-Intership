a33888606846c5ad6413a7b7347a8ea532223b52
1TreeForest <1204572840@qq.com>
2022-07-28 20:45:29 +0800

    del nginx

--- nginx-1.23.1/objs/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-SERVER_PORT=$1
-
-pid=`lsof -i:$SERVER_PORT | grep "root" | grep -v "grep" | awk '{print $2}'`
-echo $pid
-gdb attach -q $pid <<__EOF__
-p (size_t) __gcov_dump_one()
-__EOF__
